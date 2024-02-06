77aeedcb820747d0b12db9bb6d40b8493627cad1
Matvey Kommercheskiy <kommercheskiy.m@wb.ru>
2024-02-06 14:26:03 +0300

    added css.modules

--- src/components/JournalForm/JournalForm.jsx
+++ src/components/JournalForm/JournalForm.jsx
@@ -1,22 +1,56 @@
-import './JournalForm.css';
+import styles from './JournalForm.module.css';
 import React, {useState} from "react";
 import Button from "../Button/Button.jsx";
 
 function JournalForm({ onSubmit }) {
+    const [formValidState, setFormValidState] = useState({
+        title: true,
+        text: true,
+        date: true
+    });
+
     const addJournalItem = (event) => {
         event.preventDefault()
 
         const formData = new FormData(event.target);
         const formProps = Object.fromEntries(formData);
+
+        let isFormValid = true;
+        if (!formProps.title?.trim().length) {
+            setFormValidState(oldState => ({...oldState, title: false}));
+            isFormValid = false;
+        } else {
+            setFormValidState(oldState => ({...oldState, title: true}));
+            isFormValid = true;
+        }
+        if (!formProps.text?.trim().length) {
+            setFormValidState(oldState => ({...oldState, text: false}));
+            isFormValid = false;
+        } else {
+            setFormValidState(oldState => ({...oldState, text: true}));
+            isFormValid = true;
+        }
+        if (!formProps.date) {
+            setFormValidState(oldState => ({...oldState, date: false}));
+            isFormValid = false;
+        } else {
+            setFormValidState(oldState => ({...oldState, date: true}));
+            isFormValid = true;
+        }
+
+        if (!isFormValid) {
+            return
+        }
+
         onSubmit(formProps);
     };
 
     return (
-        <form className='journal-form' onSubmit={addJournalItem}>
-            <input type='text' name='title' />
-            <input type='date' name='date' />
+        <form className={styles['journal-form']} onSubmit={addJournalItem}>
+            <input type='text' name='title' className={`${styles['input']} ${formValidState.title ? '' : styles['invalid']}`} />
+            <input type='date' name='date' className={`${styles['input']} ${formValidState.date ? '' : styles['invalid']}`} />
             <input type='text' name='tag' />
-            <textarea name='text'></textarea>
+            <textarea name='text' className={`${styles['input']} ${formValidState.text ? '' : styles['invalid']}`}></textarea>
             <Button text='Сохранить' />
         </form>
     );
